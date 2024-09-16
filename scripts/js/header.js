2a9c6799bbf070e5117ea8d83089d78564aad9ac
Wenting-Yu <89863189+Wenting-Yu@users.noreply.github.com>
2024-09-16 14:19:32 -0500

    sticker header js

---
+++ scripts/js/header.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {stickyFunction()};
+
+var header = document.getElementById("main-menu-container");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
